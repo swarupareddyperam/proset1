5028356df815acd26abbbb013e53554566c5fd4f
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-16 11:43:28 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Prefix
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int min=999999;
+		int i,j,len,f=1;
+		int c=0;
+
+		String s=sc.nextLine();
+		String A[]=new String[n];
+		for(i=0;i<n;i++){
+			A[i]=sc.nextLine();
+			len=A[i].length();
+			if(len<min)
+				min=len;
+		}
+		for(i=0;i<=min-1;i++){
+			for(j=0;j<=n-2;j++){
+				if(A[j].charAt(i)==A[j+1].charAt(i))
+					continue;
+				else{
+					f=0;
+					break;
+				}
+					
+			}
+			if(f==1){
+				c++;
+				System.out.print(A[0].charAt(i));
+			}
+			else
+				break;
+
+		}
+	}
+}
+	
